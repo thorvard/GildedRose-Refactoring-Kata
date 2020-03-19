875803a6cee90232d0ce3c9ce577d48ef8a2caeb
Ender Erol <ender.em@gmail.com>
2020-03-19 16:43:21 +0100

    Added characterization test cases for update quality

--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -1,19 +1,59 @@
 package com.gildedrose
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class GildedRoseTest {
 
-    @Test fun foo() {
+    @Test
+    fun `should not change item name when quality updated`() {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
-        app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
 
+        app.updateQuality()
+
+        assertEquals("foo", app.items[0].name)
     }
 
+    @Test
+    fun `should not change empty item name when quality updated`() {
+        val items = arrayOf<Item>(Item("", 0, 0))
+        val app = GildedRose(items)
 
+        app.updateQuality()
+
+        assertEquals("", app.items[0].name)
+    }
+
+    @Test
+    fun `should not change multi-part item name when quality updated`() {
+        val items = arrayOf<Item>(Item("foo bar", 0, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals("foo bar", app.items[0].name)
+    }
+
+    @Test
+    fun `should decrease item sellIn by 1 when quality updated`() {
+        val items = arrayOf<Item>(Item("foo", 5, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(4, app.items[0].sellIn)
+    }
+
+    @Test
+    fun `should not change item sellIn when item name is Sulfuras, Hand of Ragnaros and quality updated`() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros", 5, 0))
+        val app = GildedRose(items)
+
+        app.updateQuality()
+
+        assertEquals(5, app.items[0].sellIn)
+    }
 }
 
 
